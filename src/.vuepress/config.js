c422fb6e7bb6d0fca5eaeade291fa907a4dd4843
zhangbin <471623510@qq.com>
2022-12-02 17:20:13 +0800

    feat: update file position

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   title: 'ZahirBodrike的网络日志',
-  dest: path.resolve(__dirname, '../../', 'dist'),
+  dest: path.resolve(__dirname, '../../', 'docs'),
 }
